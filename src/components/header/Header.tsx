1c3d0710276e25177e74d993ee487036375b976c
Hoang Chuong <hoangchuong300494@gmail.com>
2024-02-20 19:29:58 +0700

    fix shopping card

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import classnames from "classnames/bind";
 import styles from "./header.module.scss";
 import { useState, useEffect } from "react";
@@ -12,13 +11,12 @@ import Image from "next/image";
 import logo from "/public/logo1@2x-free-img.png";
 import Navbar from "@/components/navbar/Navbar";
 import Account from "@/components/account/Account";
-import ShopCart from "@/components/shop-cart/ShopCart";
+// import ShopCart from "@/components/shop-cart/ShopCart";
 
 const cx = classnames.bind(styles);
 
 export default function Header() {
   const currentPath = usePathname();
-  const [headerMobile, setHeaderMobile] = useState(false);
   const [headerStore, setHeaderStore] = useState("");
   const listUrl = ["/", "/about", "/contact"];
   const [menubarMobile, setMenubarMobile] = useState(false);
@@ -43,9 +41,7 @@ export default function Header() {
             <div className={cx("navbar")}>
               <Navbar />
             </div>
-            <div className={cx("wrapper-shopcard")}>
-              <ShopCart />
-            </div>
+            <div className={cx("wrapper-shopcard")}>{/* <ShopCart /> */}</div>
             <div className={cx("account")}>
               <Account />
             </div>
--- src/components/shop-cart/ShopCart.tsx
+++ src/components/shop-cart/ShopCart.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import classnames from "classnames/bind";
-import styles from "./ShopCart.module.scss";
-import { GiShoppingBag } from "react-icons/gi";
+// import React from "react";
+// import classnames from "classnames/bind";
+// import styles from "./shopCart.module.scss";
+// import { GiShoppingBag } from "react-icons/gi";
 
-const cx = classnames.bind(styles);
+// const cx = classnames.bind(styles);
 
-export default function ShopCard() {
-  return (
-    <div className={cx("container")}>
-      <span className={cx("total-price")}>$0.00</span>
-      <div className={cx("shopping-cart-icon")}>
-        <i>
-          <GiShoppingBag />
-        </i>
-        <span className={cx("product-number")}>0</span>
-      </div>
-    </div>
-  );
-}
+// export default function ShopCard() {
+//   return (
+//     <div className={cx("container")}>
+//       <span className={cx("total-price")}>$0.00</span>
+//       <div className={cx("shopping-cart-icon")}>
+//         <i>
+//           <GiShoppingBag />
+//         </i>
+//         <span className={cx("product-number")}>0</span>
+//       </div>
+//     </div>
+//   );
+// }
